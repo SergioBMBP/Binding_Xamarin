2a33133078f2e8b2c85ba532b4994902fbd7e070
sergiobameba <sergena23@gmail.com>
2019-05-06 19:40:25 +0200

    ejemplo bindings

---
+++ ProyectoBinding/ProyectoBinding/ProyectoBinding/BindingClases.xaml.cs
@@ -0,0 +1,29 @@
+﻿using ProyectoBinding.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProyectoBinding
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    
+	public partial class BindingClases : ContentPage
+	{
+        public Persona person;
+        public BindingClases ()
+		{
+			InitializeComponent ();
+            this.person = new Persona();
+            person.Nombre = "Alex Sandro";
+            person.Edad = 35;
+            person.Imagen = "http://www.juventus.com/media/images/news-images/2018-19/giocatori/sandro01.jpg";
+            this.BindingContext = person;
+        
+		}
+	}
+}
